f5ee7d2c81207528fb49b3ab0e5518887d895d8d
Rafal Wilinski <raf.wilinski@gmail.com>
2024-01-18 14:26:14 +0100

    Update sha

--- formula.rb
+++ formula.rb
@@ -1,8 +1,8 @@
 class ExampleApp < Formula
   desc "CLI allowing you to interact with AWS Cloud using human language inside your Terminal thanks to generative AI."
   homepage "https://chatwithcloud.ai"
-  url "https://github.com/your-username/example-app/releases/download/v1.0.0/example-app.tar.gz"
-  sha256 "your-sha256-checksum"
+  url "https://github.com/ChatWithCloud/homebrew/releases/download/0.3.2/chatwithcloud"
+  sha256 "97b92c89df38211acbe25fe0808f1af3bf80f88433bf6f7b9e04f308fd2d3772"
 
   def install
     bin.install "chatwithcloud"

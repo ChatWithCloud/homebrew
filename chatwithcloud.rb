2cb1f49b91ee3c218e7ad3a4cbe4bedd69cd99d8
Rafal Wilinski <raf.wilinski@gmail.com>
2024-01-18 14:31:57 +0100

    Syntax fix

---
+++ chatwithcloud.rb
@@ -0,0 +1,10 @@
+class Chatwithcloud < Formula
+  desc "CLI allowing you to interact with AWS Cloud using human language inside your Terminal thanks to generative AI."
+  homepage "https://chatwithcloud.ai"
+  url "https://github.com/ChatWithCloud/homebrew/releases/download/0.3.2/chatwithcloud"
+  sha256 "97b92c89df38211acbe25fe0808f1af3bf80f88433bf6f7b9e04f308fd2d3772"
+
+  def install
+    bin.install "chatwithcloud"
+  end
+end

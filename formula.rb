4c9c8aeab7927867e35293a10475db7d39f07326
Rafal Wilinski <raf.wilinski@gmail.com>
2024-01-18 14:23:46 +0100

    Dummy formula

---
+++ formula.rb
@@ -0,0 +1,10 @@
+class ExampleApp < Formula
+  desc "CLI allowing you to interact with AWS Cloud using human language inside your Terminal thanks to generative AI."
+  homepage "https://chatwithcloud.ai"
+  url "https://github.com/your-username/example-app/releases/download/v1.0.0/example-app.tar.gz"
+  sha256 "your-sha256-checksum"
+
+  def install
+    bin.install "chatwithcloud"
+  end
+end
